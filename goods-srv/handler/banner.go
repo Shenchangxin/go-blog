6b36e1d9d30214aa44b88a8b9882879a524d176f
Shenchangxin <2863727114@qq.com>
2024-07-23 14:51:10 +0800

    开发轮播图服务端接口

--- goods-srv/handler/banner.go
+++ goods-srv/handler/banner.go
@@ -2,21 +2,76 @@ package handler
 
 import (
 	"context"
+	"go-blog/goods-srv/global"
+	"go-blog/goods-srv/model"
 	"go-blog/goods-srv/proto"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 	"google.golang.org/protobuf/types/known/emptypb"
 )
 
-func (*GoodsServer) BannerList(context.Context, *emptypb.Empty) (*proto.BannerListResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method BannerList not implemented")
+func (s *GoodsServer) BannerList(ctx context.Context, req *emptypb.Empty) (*proto.BannerListResponse, error) {
+	bannerListResponse := proto.BannerListResponse{}
+
+	var banners []model.Banner
+	result := global.DB.Find(&banners)
+	bannerListResponse.Total = int32(result.RowsAffected)
+
+	var bannerResponses []*proto.BannerResponse
+	for _, banner := range banners {
+		bannerResponses = append(bannerResponses, &proto.BannerResponse{
+			Id:    banner.ID,
+			Image: banner.Image,
+			Index: banner.Index,
+			Url:   banner.Url,
+		})
+	}
+
+	bannerListResponse.Data = bannerResponses
+
+	return &bannerListResponse, nil
 }
-func (*GoodsServer) CreateBanner(context.Context, *proto.BannerRequest) (*proto.BannerResponse, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method CreateBanner not implemented")
+
+func (s *GoodsServer) CreateBanner(ctx context.Context, req *proto.BannerRequest) (*proto.BannerResponse, error) {
+
+	banner := model.Banner{}
+
+	banner.Image = req.Image
+	banner.Index = req.Index
+	banner.Url = req.Url
+
+	global.DB.Save(&banner)
+
+	return &proto.BannerResponse{
+		Id: banner.ID,
+	}, nil
+
 }
-func (*GoodsServer) DeleteBanner(context.Context, *proto.BannerRequest) (*emptypb.Empty, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method DeleteBanner not implemented")
+
+func (s *GoodsServer) DeleteBanner(ctx context.Context, req *proto.BannerRequest) (*emptypb.Empty, error) {
+	if result := global.DB.Delete(&model.Banner{}, req.Id); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "轮播图不存在")
+
+	}
+	return &emptypb.Empty{}, nil
+
 }
-func (*GoodsServer) UpdateBanner(context.Context, *proto.BannerRequest) (*emptypb.Empty, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method UpdateBanner not implemented")
+
+func (s *GoodsServer) UpdateBanner(ctx context.Context, req *proto.BannerRequest) (*emptypb.Empty, error) {
+
+	var banner model.Banner
+	if result := global.DB.First(&model.Banner{}, req.Id); result.RowsAffected == 0 {
+		return nil, status.Errorf(codes.NotFound, "轮播图不存在")
+
+	}
+
+	if req.Url != "" {
+		banner.Url = req.Url
+	}
+	if req.Image != "" {
+		banner.Image = req.Image
+	}
+	global.DB.Save(&banner)
+	return &emptypb.Empty{}, nil
+
 }
